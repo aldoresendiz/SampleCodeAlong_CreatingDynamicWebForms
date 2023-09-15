be2afa8950f4172268d926ffa958ab436ed3188f
Aldo Resendiz <aldoresendiz@gmail.com>
2023-09-14 22:11:03 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const header = document.createElement('h1');
+header.textContent = 'Warranty Information';
+document.body.appendChild(header);
+
+function createObject(content) {
+    const objeto = document.createElement('label');
+    objeto.textContent = content;
+
+    const objetoI = document.createElement('input');
+    objetoI.id = String(content).toLowerCase().replace(' ', '');
+    
+    objeto.style.display = objetoI.style.display = 'block';
+    document.body.append(objeto, objetoI);
+    console.log(objeto, objetoI);
+
+}
+const elements = ['First Name', 'Last Name', 'Company Name', 'Email', 'Phone Number', 'Fax Number', 'Address', 'City', 'State', 'Zip', 'Country', 'Unit Information', 'Date of Purchase', 'Model', 'Warranty Serial Number'];
+
+elements.forEach((elemento) => {
+    createObject(elemento);
+});
+
+const saveinfo = document.createElement('button');
+saveinfo.textContent = 'Save';
+document.body.append(saveinfo);
+const closeform = document.createElement('button');
+closeform.textContent = 'Close';
+document.body.append(closeform);
